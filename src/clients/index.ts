2ead5444a4862444e9456b23835159885285829d
gasser1000 <jbubar9@gmail.com>
2025-02-06 12:15:46 -0500

    discord removal

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -1,5 +1,4 @@
 import { AutoClientInterface } from "@elizaos/client-auto";
-import { DiscordClientInterface } from "@elizaos/client-discord";
 import { TelegramClientInterface } from "@elizaos/client-telegram";
 import { TwitterClientInterface } from "@elizaos/client-twitter";
 import { Character, IAgentRuntime } from "@elizaos/core";
@@ -14,10 +13,6 @@ export async function initializeClients(
   if (clientTypes.includes("auto")) {
     const autoClient = await AutoClientInterface.start(runtime);
     if (autoClient) clients.push(autoClient);
-  }
-
-  if (clientTypes.includes("discord")) {
-    clients.push(await DiscordClientInterface.start(runtime));
   }
 
   if (clientTypes.includes("telegram")) {
